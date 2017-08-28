6f8cdab10e8d2f59e6fec6161ea2697b6dfff0f6
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:51:03 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import thenP from "./source"
+import thenP from "./"
 
 test(({equal}) => {
   return thenP((value) => equal(value, "a"))(Promise.resolve("a"))
