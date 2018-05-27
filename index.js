6149883630a8f6362148d07f02aec2c6088cfc09
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export default function thenP (resolution: any => any): Function {
-  return function thenPResolution (promise: Promise<any>): Promise<any> {
-    return promise.then(resolution)
-  }
+export default function thenP(resolution) {
+  return function thenPResolution(promise) {
+    return promise.then(resolution);
+  };
 }
